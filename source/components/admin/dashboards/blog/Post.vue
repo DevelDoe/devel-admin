e549ce7dc0e314e6557564465d0a24f38f3e2f16
develdoe <andreeray@live.com>
2018-10-26 04:50:13 +0200

    added vue to categories

--- source/components/admin/dashboards/blog/Post.vue
+++ source/components/admin/dashboards/blog/Post.vue
@@ -9,7 +9,7 @@ export default {
     name: 'post',
     data() {
         return {
-            categories: [ 'HTML', 'CSS', 'JavaScript', 'Linux', 'MongoDB', 'Git' ]
+            categories: [ 'HTML', 'CSS', 'JavaScript', 'Linux', 'MongoDB', 'Git', 'Vue' ]
         }
     },
     computed: {
