aba9581066f00e1b6a0f9144a9f291ab138a26ce
develdoe <andreeray@live.com>
2018-10-30 13:45:08 +0100

    bug fix publishedAt

--- source/components/admin/dashboards/blog/Post.vue
+++ source/components/admin/dashboards/blog/Post.vue
@@ -26,7 +26,6 @@ export default {
                     summary: '',
                     original: '',
                     body: '',
-                    publishedAt: '',
                     category: '',
                     tags: [],
                     published: false,
