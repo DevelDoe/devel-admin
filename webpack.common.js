4dba983993b62fac4199acbe2b731793842dafd8
develdoe <andreeray@live.com>
2018-11-07 14:26:15 +0100

    title

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
     plugins: [
         new WriteFilePlugin(),
         new HtmlWebpackPlugin({
-            title: 'devel admin',
+            title: 'develdevils',
             template: 'index.html'
         }),
         new webpack.ProvidePlugin({
