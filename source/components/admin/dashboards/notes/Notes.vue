0a2a03851e2bff51d19da387bb7f368a432346fa
develdoe <andreeray@live.com>
2018-11-07 15:24:17 +0100

    fixed notes

--- source/components/admin/dashboards/notes/Notes.vue
+++ source/components/admin/dashboards/notes/Notes.vue
@@ -164,7 +164,7 @@ export default {
                             }
                         }
                         .fa-done {
-                            color: #eee;
+                            color: #eee !important;
                         }
                     }
                     .edit {
