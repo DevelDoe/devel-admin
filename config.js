4508de453a64945406e3e96d483af55d0a96ece4
develdoe <andreeray@live.com>
2018-12-16 22:41:27 +0100

    u

--- config.js
+++ config.js
@@ -1,13 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
     app_name: 'aquar',
     api_url: 'http://35.210.92.246:4003',
     web_socket: 'ws://35.210.92.246:4003',
     target_site: 'aquar'
-=======
-    app_name: 'Devel Admin',
-    api_url: 'http://35.210.92.246:4000',
-    web_socket: 'ws://35.210.92.246:4000',
-    target_site: 'Devel Admin'
->>>>>>> master
 }
