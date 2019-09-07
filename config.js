bd8f8486c44beb3ef807fb6136c3ab7000b63c4f
develdoe <andreeray@live.com>
2019-09-07 13:15:22 +0200

    u

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 module.exports = {
-    app_name: 'Devel Admin',
-    api_url: 'http://35.210.92.246:4000',
-    web_socket: 'ws://35.210.92.246:4000',
-    target_site: 'Devel Admin'
+    app_name: 'Wimse Admin',
+    app_intro: 'login to administer your site',
+    api_url: 'http://35.210.92.246:4006',
+    web_socket: 'ws://35.210.92.246:4006',
+    target_site: 'Wimse',
+    social_facebook: '',
+    social_twitter: '',
+    social_instagram:'',
+    social_website: '',
+    social_github: '',
 }
