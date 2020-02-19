a57aca234fa00f941249385d885d1bd9661755af
develdoe <andreeray@live.com>
2020-02-19 11:34:33 +0100

    u

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    app_name: 'DEVEL DEVILS',
+    app_name: 'Admin Wimse',
     app_intro: '',
     api_url: 'http://35.210.92.246:4002',
     web_socket: 'ws://35.210.92.246:4002',
-    target_site: 'log.develdevils',
+    target_site: 'log.wimse',
     portal_button: '',
     social_facebook: 'https://www.facebook.com/develdoe',
     social_twitter: '',
