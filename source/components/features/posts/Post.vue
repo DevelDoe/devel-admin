8a6094c6ea5a1f3b41a916d16eeb4ad6aa6802e8
thomasfors <wimse.thomasfors@gmail.com>
2020-05-22 00:41:29 +0200

    post

--- source/components/features/posts/Post.vue
+++ source/components/features/posts/Post.vue
@@ -7,21 +7,47 @@
             <div class="col child">
 
                 <form id="userForm" :class="{ 'needs-validation': !valid }" novalidate onsubmit="return false;">
-                
-                    <div class="form-group">
-                        
-                        <label>Title</label>
-                        <!-- TODO: remove force -->
-                        <input type="text" class="form-control" id="title"  placeholder="enter a title here" v-model="post.title" @keydown="$forceUpdate()">
+
+                    <div class="row post-title">
+
+                        <div class="col-4">
+
+                            <div class="form-group">
+                                <select class="form-control"  v-model="post.category" @blur="$forceUpdate()">
+                                    <option disabled value="">CATEGORY</option>
+                                    <option v-for="(key, i) in select.category" :value="key" :key="'category'+i">{{key}}</option>
+                                </select>
+                            </div>
+
+                        </div>
+
+
+                        <div class="col">
+
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="title"  placeholder="TITLE" v-model="post.title" @keydown="$forceUpdate()" autocomplete="off">
+                            </div>
+
+                        </div>
 
                     </div>
 
-                    <div class="form-group">
-                        <label>Category</label>
-                        <select class="form-control"  v-model="post.category" @blur="$forceUpdate()">
-                            <option disabled value="">Choose a category</option>
-                            <option v-for="(key, i) in select.category" :value="key" :key="'category'+i">{{key}}</option>
-                        </select>
+                    <div class="row body">
+
+                        <div class="col">
+
+                            <div class="form-group" >
+                                <textarea class="form-control" id="summary" placeholder="summary" v-model="post.summary" @keydown="$forceUpdate()" autocomplete="off"></textarea>
+                            </div>
+
+                            <div class="form-group" style="height:100%">
+                                <textarea class="form-control" id="body" placeholder="body" v-model="post.body" @keydown="$forceUpdate()" autocomplete="off" ></textarea>
+                            </div>
+
+                            
+
+                        </div>
+
                     </div>
                     
                 </form>
@@ -29,12 +55,40 @@
             </div>
 
             <!-- PREVIEW -->
-            <div class="col-lg-6" id="blogPreview">
+            <div class="col" >
 
-                <div class="child" id="blogPreviewChild">
+                <div class="row">
+
+                    <div class="col">
+
+
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="tags"  placeholder="comma separated tags" v-model="post.tags" @keydown="$forceUpdate()" autocomplete="off">
+                        </div>
+
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="original" placeholder="Refference" v-model="post.original" @keydown="$forceUpdate()" autocomplete="off">
+                        </div>
+
+                        <div class="form-group">
+                            <select class="form-control"  v-model="post.category" @blur="$forceUpdate()">
+                                <option disabled value="">language</option>
+                                <option v-for="(key, i) in select.lang" :value="key" :key="'lang'+i">{{key}}</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group" v-if="post.published">
+                            <datepicker placeholder="date" v-model="post.date"></datepicker>
+                        </div>
+
+                    </div>
+                
+                </div>
+
+                <!-- <div class="child" id="blogPreviewChild">
 
                     <header id="header">
-                        <h1>{{ post.category}} - {{ post.title }}</h1>
+                        <h1>{{ post.category}}<span v-if='post.title'>, </span>{{ post.title }}</h1>
                     </header>
                     
                     <div id="summary">
@@ -45,22 +99,42 @@
                         <p v-html="$markdown.render(post.body)"></p>
                     </div>
 
-                </div>
+                </div> -->
 
             </div>
 
         </div>
 
-        
-
-        <hr>
-
-        <gForm :schema="'post'" :data="post" :select="select" />
+      
+        <div class="row controls" v-if="post._id">
+            <div class="btn-group">
+    
+                <button class="btn btn-ctrl" :class="{ 'active': post.published }"  @click="post.published = !post.published, $forceUpdate()" >published</button>
+                <button class="btn btn-ctrl" :class="{ 'active': post.shared }"  @click="post.shared = !post.shared, $forceUpdate()" >shared</button>
+                <button class="btn btn-ctrl" :class="{ 'active': post.wip }"  @click="post.wip = !post.wip, $forceUpdate()" >wip</button>
+                <button class="btn btn-ctrl" :class="{ 'active': post.feat }"  @click="post.feat = !post.feat, $forceUpdate()" >feat</button>
+                <button class="btn btn-ctrl" :class="{ 'active': post.important }"  @click="post.important = !post.important, $forceUpdate()" >important </button>
+                <button type="button" class="btn btn" @click="update()">Save</button>
+                <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal">Delete</button>
+            </div>
+        </div>
+        <div class="row controls" v-else>
+            <div class="btn-group">
+                <button class="btn btn-ctrl" :class="{ 'active': post.published }"  @click="post.published = !post.published, $forceUpdate()" >published</button>
+                <button class="btn btn-ctrl" :class="{ 'active': post.shared }"  @click="post.shared = !post.shared, $forceUpdate()" >shared</button>
+                <button class="btn btn-ctrl" :class="{ 'active': post.wip }"  @click="post.wip = !post.wip, $forceUpdate()" >wip</button>
+                <button class="btn btn-ctrl" :class="{ 'active': post.feat }"  @click="post.feat = !post.feat, $forceUpdate()" >feat</button>
+                <button class="btn btn-ctrl" :class="{ 'active': post.important }"  @click="post.important = !post.important, $forceUpdate()" >important </button>
+                <button v-if="valid" type="button" class="btn" @click="save()">Save</button>
+                <button v-else type="button" class="btn" disabled>Save</button>
+            </div>
+        </div> 
 
     </div>
 </template>
 <script>
 import {mapGetters} from 'vuex'
+import Datepicker from 'vuejs-datepicker'
 export default {
     name: 'post',
     page: 'new post',
@@ -97,6 +171,13 @@ export default {
                 }
             }
         },
-    }
+    },
+    components: {
+        Datepicker,
+    },
+    updated() {
+        var body = document.getElementById("blogPreviewChild")
+        body.scrollTop = body.scrollHeight 
+    },
 }
 </script>
